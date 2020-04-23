45befbda67a0bae6f14f90b956b3053430f7ba16
Sandie Nunez <snunez@vt.edu>
2020-04-23 23:46:22 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,26 @@
+require "pry"
+class MP3Importer
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files 
+   file_array =  Dir.glob("#{path}/*.mp3")
+  file_array.collect do |file| #collect returns array 
+    file.slice!("#{path}/")
+    file 
+    
+  end 
+end 
+  
+ def import 
+       files.each do |file|
+         Song.new_by_filename(file) 
+             #binding.pry
+      #song.new 
+        end 
+ end 
+  
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist 
   
   @@all = [] 
-  def initialize(name)
+  def initialize(name, artist = nil)
     @name = name 
     @artist = artist 
     @@all << self 
@@ -12,9 +12,19 @@ class Song
     @@all 
   end 
   
-  def Song.new_by_filename
-    
+  def Song.new_by_filename(song)
+    #binding.pry
+    song_string = song.split(" - ")[1]
+    song_artist_string = song.split(" - ")[0] #tricky #interview #use descriptive names 
+    new_artist = Artist.new(song_artist_string) #making a new artist
+    #binding.pry
+    song_obj = Song.new(song_string, new_artist) #we want artist to be an actual artist object not just a string 
+   song_obj #we want the return of method to be new song obj we created
   end 
   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name) 
+    
+  end 
   
 end 
