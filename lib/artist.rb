03aad7e3a047cc1a043694294eb3d3ec754cf982
Sandie Nunez <snunez@vt.edu>
2020-04-23 05:33:20 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,11 +32,13 @@ class Artist
 end 
   
   def print_songs
-    
+  Song.all.select do |song| #have instance inside pipes 
+   if song.artist == self 
+    puts "#{song.name}"
   end 
-   
+ end   
 end 
 
-
+end 
 ##take instance of song and calling the attribtue accessor of song class #self means the instance of the artist #self is the artist instance 
 ##received an instance aka argument
