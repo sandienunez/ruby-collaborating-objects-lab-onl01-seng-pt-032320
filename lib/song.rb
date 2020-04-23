4e1439354bcaf8c45304daab52505ac75d30f3bc
Sandie Nunez <snunez@vt.edu>
2020-04-23 23:30:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     song_string = song.split(" - ")[1]
     song_artist_string = song.split(" - ")[0] #tricky #interview #use descriptive names 
     song_obj = Song.new(song_string, song_artist_string)
-    #artist = song_artist 
+   song_obj #we want the return of method to be new song obj we created
   end 
   
   
