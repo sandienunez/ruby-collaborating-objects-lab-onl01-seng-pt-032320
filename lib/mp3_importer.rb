dea98fbd4d4544c1647784e0043327e45dea8102
Sandie Nunez <snunez@vt.edu>
2020-04-23 22:51:13 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,11 +7,12 @@ class MP3Importer
   end 
   
   def files 
+    binding.pry 
    file_array =  Dir.glob("#{path}/*.mp3")
   file_array.collect do |file| #collect returns array 
     file.slice!("#{path}/")
     file 
-    #binding.pry 
+    
   end 
 end 
   
