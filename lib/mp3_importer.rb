e0478be3889cbcbf2bdeae52ddb2c583fba6f6b1
Sandie Nunez <snunez@vt.edu>
2020-04-23 22:40:12 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+require "pry"
+class MP3Importer
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files 
+   file_array =  Dir.glob("#{path}/*.mp3")
+  file_array.collect do |file| #collect returns array 
+    file.slice!("#{path}/")
+    file 
+    #binding.pry 
+  end 
+  
+end 
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,10 @@ class Song
   end 
   
   def Song.new_by_filename
-    
+    song_string = file_name.split(" - ")[1]
+    song_obj = Song.new(song_string)
+    song_artist_string = file_name.split(" - ")[0]  #tricky #interview #use descriptive names 
+    artist = song_artist 
   end 
   
   
