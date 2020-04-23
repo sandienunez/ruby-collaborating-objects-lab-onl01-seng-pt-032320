3ad7fd68cdef6199e4fbd3fc897a100a173030ef
Sandie Nunez <snunez@vt.edu>
2020-04-23 23:36:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     #binding.pry
     song_string = song.split(" - ")[1]
     song_artist_string = song.split(" - ")[0] #tricky #interview #use descriptive names 
-    song_artist_string = Artist.new()
+    new_artist = Artist.new(song_artist_string) #making a new artist
     #binding.pry
-    song_obj = Song.new(song_string, song_artist_string)
+    song_obj = Song.new(song_string, new_artist) #we want artist to be an actual artist object not just a string 
    song_obj #we want the return of method to be new song obj we created
   end 
   
